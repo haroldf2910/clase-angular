383fba62411641a2c3a6acbe7c03c9f1070bd2fc
harold <haroldf000@gmail.com>
2023-07-13 18:49:28 -0500

    Primera Clase bidi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentesComponent } from './componentes/componentes.component';
+import { BindingComponent } from './clase/binding/binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentesComponent,
+    BindingComponent
   ],
   imports: [
     BrowserModule,
